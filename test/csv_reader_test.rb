df8851d6266721433d79a17be78fcd17716281a5
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-15 14:26:48 -0600

    csv reader reads csvs with file path passed in

--- test/csv_reader_test.rb
+++ test/csv_reader_test.rb
@@ -2,11 +2,19 @@ require_relative 'test_helper'
 require_relative '../lib/csv_reader'
 
 class CsvReaderTest < Minitest::Test
-
-  def test_csv_reader_exists
-    csv = CsvReader.new
-    assert csv
+  def setup
+    @csv = CsvReader.new('./data/plans.csv')
   end
 
+  def test_csv_reader_exists
+    assert_instance_of CsvReader, @csv
+  end
 
+  def test_csv_reader_takes_a_file_path
+    assert @csv.file_path, '../data/plans.csv'
+  end
+
+  def test_csv_reader_opens_csv
+    assert @csv.open_csv, CSV
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'pry'
 require 'minitest/pride'
 require 'minitest/autorun'
+require 'csv'
